2f0eac59afe8f32bd2e131e559e12420ab7bf76c
Root User <root@localhost>
2016-10-09 00:03:30 +0100

    Change
    Change

--- src/main/java/com/example/HolaSpringbootApplication.java
+++ src/main/java/com/example/HolaSpringbootApplication.java
@@ -1,6 +1,7 @@
 package com.example;
 
-import org.springframework.boot.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
