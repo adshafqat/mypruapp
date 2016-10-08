dc9d09ffea3df46feb07cf7f7cf2c32c2898fa31
Root User <root@localhost>
2016-10-09 00:01:22 +0100

    Change

--- src/main/java/com/example/HolaSpringbootApplication.java
+++ src/main/java/com/example/HolaSpringbootApplication.java
@@ -1,10 +1,10 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HolaSpringbootApplication {
+public class HolaSpringbootApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolaSpringbootApplication.class, args);
